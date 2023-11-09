5dc41ac5127301436491b15a044847916793642d
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-11-10 00:01:41 +0900

    Title: 정수 입력받아 배수 출력, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231109/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=5;i++){
+            System.out.printf("%d ",i*n);
+        }
+    }
+}
