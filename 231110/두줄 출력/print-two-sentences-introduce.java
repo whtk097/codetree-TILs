edd431883cc1fd4431793012266746d62a5c96b7
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-11-10 17:30:00 +0900

    Title: 두줄 출력, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231110/두줄 출력/print-two-sentences-introduce.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        System.out.println("Hello students!");
+        System.out.println("Welcome to LeebrosCode!");
+
+    }
+}
