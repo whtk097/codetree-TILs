63771aac44c0dd0b9531abb04592df82967323cb
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-11-10 17:10:01 +0900

    Title: n부터 1까지 숫자 출력, Time: 169ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231110/n부터 1까지 숫자 출력/print-n-to-1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=n;
+        while(i>=1){
+            System.out.printf("%d ",i);
+            i--;
+        }
+    }
+}
