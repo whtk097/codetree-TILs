4f95c213315c5cf93ba9b8c56b589c57562413d9
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-12-25 23:01:13 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231225/특정 조건 배수의 합/sum-of-specific-condition-multiple.c
+++ 231225/특정 조건 배수의 합/sum-of-specific-condition-multiple.c
@@ -6,8 +6,14 @@ int main() {
     int sum=0;
 
     scanf("%d %d",&a,&b);
+    if(a<b){
     for(int i=a;i<=b;i++){
         if(i%5==0) sum+=i;
+    }}
+    else{
+        for(int i=b;i<=a;i++){
+        if(i%5==0) sum+=i;
+    }
     }
     printf("%d",sum);
     return 0;
