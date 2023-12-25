ee6d6afcccb89ff633f5d7e1a04296cec088446e
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-12-25 23:13:07 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231225/완전수 판별기/perfect-number-discriminator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n=0;int sum=0;
+    scanf("%d",&n);
+    for(int i=1;i<n;i++){
+        if(n%i==0) sum+=i;
+        
+    }
+    if(sum==n) printf("P");
+        else printf("N");
+    return 0;
+}
