d358a92da4282a69330248a692d28acdb618be64
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-12-27 21:58:59 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a=0;
+    while(1){
+        scanf("%d",&a);
+        if(a!=0)
+        printf("%d\n",a);
+        else break;
+    }
+    return 0;
+}
