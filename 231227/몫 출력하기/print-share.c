31319980c3790d2ef7111b8d6c97d926ca6f5b8b
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-12-27 22:28:43 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231227/몫 출력하기/print-share.c
+++ 231227/몫 출력하기/print-share.c
@@ -4,18 +4,15 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a=0;
     
-    
-    scanf("%d",&a); int sum=0;
-    if(a%2==1) return 0;
+     int sum=0; int cnt=0;
+    while(cnt<3){
+        scanf("%d",&a);
+    if(a%2==1);
     else{
-        printf("%d\n",a/2);
-        for(int i=0;i<3;i++){
-            scanf("%d",&sum);
-            if(sum%2==1) continue;
-            else{
-            sum/=2;
-            printf("%d\n",sum);}
+            printf("%d\n",a/2);
+            cnt++;
+            }
         }
-    }
+    
     return 0;
 }
