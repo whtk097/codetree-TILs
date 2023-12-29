5239f6f3d3022551f535ac2127c1ff405ce83ced
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-12-29 11:26:11 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/ab 사이에 없는 c/c-between-a-and-b-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    for(int i=a;i<=b;i++){
+        if(i%c==0){
+            printf("NO");
+            return 0;
+        }
+
+    }
+    printf("YES");
+    return 0;
+}
