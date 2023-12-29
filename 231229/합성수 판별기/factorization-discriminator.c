b07fc7f8e9da8b3622c692b29b68766245b30295
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-12-29 11:18:20 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/합성수 판별기/factorization-discriminator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            printf("C");
+            return 0;
+        }
+    }
+    printf("N");
+    return 0;
+}
