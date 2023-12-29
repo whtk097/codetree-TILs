2de7813b0ffa8888cdd18b9c511ad1202ab13e43
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-12-29 11:49:01 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231229/직각 삼각형 출력/Right-triangle-and-output.c
+++ 231229/직각 삼각형 출력/Right-triangle-and-output.c
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d",&n);
-    for(int i=1;i<=n+2;i+=2){
+    for(int i=1;i<=n*2;i+=2){
         for(int j=0;j<i;j++){
             printf("*");
         }
