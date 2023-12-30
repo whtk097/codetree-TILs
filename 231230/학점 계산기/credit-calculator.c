717447c4532aa372b5843243c90b173f7504a6b0
whtk097 <131607427+whtk097@users.noreply.github.com>
2023-12-30 21:53:17 +0900

    Title: 학점 계산기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231230/학점 계산기/credit-calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n=0;
+    scanf("%d",&n);
+    float a[10];
+    float sum=0;int i;
+    for( i=0;i<n;i++){
+        scanf("%f",&a[i]);
+        sum+=a[i];
+    }
+    printf("%.1f\n",(float)sum/n);
+    if((float)sum/n>=4){
+        printf("Perfect");
+
+    }
+    else if((float )sum/n>=3) printf("Good");
+    else printf("Poor");
+    return 0;
+}
