cbe7b985ecfd40900ba518132f29640b88285451
whtk097 <131607427+whtk097@users.noreply.github.com>
2024-01-02 13:07:20 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/구구단 만들기 2/print-multiplication-table-2.c
+++ 240102/구구단 만들기 2/print-multiplication-table-2.c
@@ -4,10 +4,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     scanf("%d %d",&a,&b);
-    for(int i=a;i<=8;i+=2){
-        for(int j=b;j>1;j--){
+    for(int i=2;i<=8;i+=2){
+        for(int j=b;j>=a;j--){
             printf("%d * %d = %d",j,i,j*i);
-            if(j!=2)printf(" / ");
+            if(j!=a)printf(" / ");
         }
         printf("\n");
     }
