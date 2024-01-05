6e2c45bce81a59f56b56614a487a8d3d4eca5113
whtk097 <131607427+whtk097@users.noreply.github.com>
2024-01-05 21:46:06 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/알파벳 출력하기/print-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a='A';
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            printf("%c",a);
+            a++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
