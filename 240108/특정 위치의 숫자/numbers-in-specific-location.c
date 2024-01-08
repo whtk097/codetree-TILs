c9e5db4a944d49d32b5e6de1ecebbe56eeac8a82
whtk097 <131607427+whtk097@users.noreply.github.com>
2024-01-08 13:23:57 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/특정 위치의 숫자/numbers-in-specific-location.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a[10];
+    for(int i=0;i<10;i++){
+        scanf("%d",&a[i]);
+        
+    }
+    printf("%d",a[2]+a[4]+a[9]);
+    return 0;
+}
