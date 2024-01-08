f7bb407bf0f40850901698e9cb1c295916976f35
whtk097 <131607427+whtk097@users.noreply.github.com>
2024-01-08 12:22:29 +0900

    Title: 완전수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240108/완전수/perfect-number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n; int cnt=0,sum=0,a,b;
+    scanf("%d %d",&a,&b);
+for(int j=a;j<=b;j++){
+    for(int i=1;i<j;i++){
+        if(j%i==0)sum+=i;
+    
+    }
+    if(sum==j) cnt++;
+}
+printf("%d",cnt);
+    return 0;
+}
