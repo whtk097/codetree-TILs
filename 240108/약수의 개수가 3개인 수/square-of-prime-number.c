8edd06b14b719773783f1058009b975ec9470af2
whtk097 <131607427+whtk097@users.noreply.github.com>
2024-01-08 12:55:18 +0900

    Title: 약수의 개수가 3개인 수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/약수의 개수가 3개인 수/square-of-prime-number.c
+++ 240108/약수의 개수가 3개인 수/square-of-prime-number.c
@@ -6,10 +6,12 @@ int main() {
     scanf("%d %d",&a,&b);
     for(int i=a;i<=b;i++){
         sum=0; 
-        for(int j=1;j<i;j++){
-            if(i%j==0)sum++;
+        for(int j=1;j<=i;j++){
+            if(i%j==0)
+            sum++;
         }
-        if(sum==3)cnt++;
+        if(sum==3) 
+        cnt++;
     }
     printf("%d",cnt);
     return 0;
