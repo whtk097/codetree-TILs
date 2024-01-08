7b59b159a141cf43291deb186a386e24a5757e6f
whtk097 <131607427+whtk097@users.noreply.github.com>
2024-01-08 15:57:31 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/특정 위치의 문자 2/char-in-specific-location-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a[10];
+    for(int i=0;i<10;i++){
+        scanf(" %c",&a[i]);
+
+    }
+    printf("%c %c %c",a[1],a[4],a[7]);
+    return 0;
+}
